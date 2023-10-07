0c8db718cd50ce9b8e038e621d54fa4d72ad41fc
sghufranmehdi <sgmehdi110@gmail.com>
2023-10-07 17:35:45 +0500

    superheroes npm package used

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-var generateName = require("sillyname");
+//var generateName = require("sillyname");
+
+// changing our code to ecmaScript modules
+/*
+import generateName from sillyname;
 var sillyName = generateName();
 
 console.log(`My name is ${sillyName}.`);
+
+
+*/
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+console.log(`My name is ${name}.`);
